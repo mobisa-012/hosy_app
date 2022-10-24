693f692c19c63c194b15bf339f02953ad22be187
mobisa-012 <mobisakwamboka@gmail.com>
2022-10-24 21:44:59 +0300

    exceptions

---
+++ lib/core/extensions/exceptions.dart
@@ -0,0 +1,9 @@
+class CustomFirebaseException implements Exception {
+  final String message;
+
+  CustomFirebaseException(this.message);
+  @override
+  String toString() {
+    return message;
+  }
+}
